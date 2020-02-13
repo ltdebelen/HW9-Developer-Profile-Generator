8bc2623a4550774f0d9f11d14ef06bfd3e77705b
Lawrence Adrian De Belen <ltdebelen@gmail.com>
2020-02-12 22:42:13 -0600

    Querying data from github using axios

--- index.js
+++ index.js
@@ -8,14 +8,48 @@ inquirer
       type: "input",
       message: "What is your GitHub username?",
       name: "username"
-    },
-    {
-      type: "list",
-      message: "What is your favorite color?",
-      name: "color",
-      choices: ["green", "blue", "pink", "red"]
     }
+    // ,
+    // {
+    //   type: "list",
+    //   message: "What is your favorite color?",
+    //   name: "color",
+    //   choices: ["green", "blue", "pink", "red"]
+    // }
   ])
   .then(function(response) {
-    console.log(response);
+    // Query Github Data
+    const username = response.username;
+    const queryURL = `https://api.github.com/users/${username}`;
+
+    axios.get(queryURL).then(function(result) {
+      // Profile Image
+      // Username
+      // User location via Google Maps
+      // User GitHub Profile
+      // User blog
+      // User bio
+      // Number of public repositories
+      // Number of followers
+      // Number of GitHub stars
+      // Number of users following
+
+      const userProfile = {
+        profile_img: result.data.avatar_url,
+        username: result.data.login,
+        location: result.data.location,
+        blog: result.data.blog,
+        bio: result.data.bio,
+        num_repo: result.data.public_repos,
+        num_followers: result.data.followers,
+        num_stars: result.data.public_gists,
+        num_following: result.data.following
+      };
+
+      console.log(userProfile);
+    });
+
+    // Pass Data to Generate HTML function
+    // Create HTML File
+    // Export HTML file to PDF
   });
